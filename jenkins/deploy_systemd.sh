78e4e97ec3ae22f3a60eb0c580a0099f83ae9de7
Tayo <t.paseda@warwick.ac.uk>
2021-03-12 14:53:57 +0000

    Update deploy_systemd.sh

--- jenkins/deploy_systemd.sh
+++ jenkins/deploy_systemd.sh
@@ -14,7 +14,7 @@ Description=Todo List
 User=pythonadm
 Environment=SECRET_KEY
 Environment=DATABASE_URI
-ExecStart=/home/jenkins/todo-list/startup.sh
+ExecStart=/home/jenkins/todo-list-app/startup.sh
 
 [Install]
 WantedBy=multi-user.target
